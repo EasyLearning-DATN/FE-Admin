5309a507d8e52c467cad3f858c21b22e5f751727
PhanQuiDuc <phanquiduc@gmail.com>
2024-02-23 14:23:59 +0700

    Add i18n and translate dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app/app.component.html',
-  styleUrls: ['./app/app.component.css']
+    selector: 'app-root',
+    templateUrl: './app/app.component.html',
+    styleUrls: ['./app/app.component.css']
 })
 export class AppComponent {
-  title = 'FE-Admin';
+    title = 'FE-Admin';
+
+    constructor(private translate: TranslateService) {
+        translate.setDefaultLang('vi');
+        translate.use('vi');
+    }
 }
