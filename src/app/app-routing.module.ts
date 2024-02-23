bbd03bc68094ac377c69017a8c23b7df7e5d2a48
AnhDT <anhdt@anhcoder.net>
2024-02-23 15:45:36 +0700

    Admin UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { RouterModule, Routes, UrlSegment } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+
+
+const routes: Routes = [
+
+  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent},
+  { path: 'login', component: LoginComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    bindToComponentInputs: true,
+    urlUpdateStrategy: 'eager',
+    paramsInheritanceStrategy: 'always',
+    scrollPositionRestoration: 'top', // Chỉ định vị trí cuộn sau khi chuyển hướng
+    anchorScrolling: 'enabled',
+  })],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
